209692777914e52c846f744168cc5771e709d506
danielpadkins <danielpadk@gmail.com>
2023-03-07 17:10:28 -0500

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>House of Nouns API</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/collectivexyz/hon-api-docs",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/collectivexyz/hon-api-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "House of Nouns API Documentation - by collective.xyz",
   },
-}
+};
 
-export default config
+export default config;
